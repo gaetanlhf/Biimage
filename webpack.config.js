674e629b841b7e28fe2250cb094409c41d25e3aa
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-04-06 22:46:25 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,44 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/js/script.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '',
-        filename: 'bundle.js'
+        publicPath: '/',
+    },
+    optimization: {
+        realContentHash: false,
     },
     mode: 'production',
-
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
-                }
+                test: /\.css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    'html-loader'
+                ]
             }
-        }, {
-            test: /\.css$/,
-            use: [{
-                    loader: MiniCssExtractPlugin.loader
-                },
-                {
-                    loader: "css-loader",
-                },
-                {
-                    loader: "postcss-loader"
-                }
-            ]
-        }]
+        ]
     },
-
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "index.html"
         })
     ]
 };
